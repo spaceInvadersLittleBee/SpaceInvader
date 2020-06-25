6f0e61a4842613c3e36b3a43e93099cb971b04d9
ge35wot <ge35wot@mytum.de>
2020-06-25 17:24:25 +0200

    no message

--- SpaceInvaders/src/test/java/Controller/AudioPlayer.java
+++ SpaceInvaders/src/test/java/Controller/AudioPlayer.java
@@ -1,6 +1,15 @@
 package Controller;
 
+import java.io.File;
+
 public class AudioPlayer {
+
+    private File bulletSound = new File("src/main/resources/bulletSound.wav");
+    private File levelUpSound = new File("src/main/resources/levelUpSound.wav");
+    private File deathSound = new File("src/main/resources/deathSound.wav");
+    private File hitmarkerSound = new File("src/main/resources/hitmarkerSound.wav");
+    private File bonusSound = new File("src/main/resources/bonusSound.wav");
+    private File backgroundMusic = new File("src/main/resources/BGM.wav");
 	
 	public void playMusic() {
 		
